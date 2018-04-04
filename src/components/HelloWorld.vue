5adea807ac17e2c448f8ada3b370809eaadd81ca
Sean Gee <seangee@gmail.com>
2018-04-04 00:20:24 -0700

    teaser

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,15 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="teaser-container">
+      <div class="teaser">
+        <div class="teaser__title">
+          {{ msg }}
+        </div>
+        <div class="teaser__blurb">
+          More to come
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -88,7 +18,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Sean and Alice Wedding'
+      msg: 'Sean & Alice'
     }
   }
 }
@@ -96,18 +26,36 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.hello {
+  height: 100vh;
+  background: url('./../assets/F01A1952.jpg') no-repeat center center fixed;
+  background-size: cover;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.teaser-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-direction: column;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.teaser {
+  color: white;
+  text-shadow: 0 4px 12px rgba(0,0,0,.26);
+  text-align: center;
 }
-a {
-  color: #42b983;
-}
+
+  .teaser__title {
+    font-size: 56px;
+    font-weight: 600;
+  }
+
+  .teaser__blurb {
+    font-size: 24px;
+    font-weight: 400;
+    opacity: 0.6;
+  }
+
 </style>
